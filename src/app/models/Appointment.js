40cb70ca75d49f4708d205d813b62cd7c39ede84
filliperomero <fillipe.romero@gmail.com>
2019-08-26 23:20:48 -0300

    Add: Appointment Controller/route/model

---
+++ src/app/models/Appointment.js
@@ -0,0 +1,26 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Appointment extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        date: Sequelize.DATE,
+        canceled_at: Sequelize.DATE,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    // When we have 2 relationships, we must declare 'as', otherwise,
+    // sequelize will not handle correct.
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
+  }
+}
+
+export default Appointment;
