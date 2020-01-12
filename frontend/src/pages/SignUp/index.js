ba3173845599cda4e51e05186c29873ec849c83a
filliperomero <fillipe.romero@gmail.com>
2020-01-12 18:36:44 -0300

    chore: configure sagas, actions, reducers and axios

--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  fullname: Yup.string().required('Nome completo obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('E-mail obrigatório'),
+  password: Yup.string()
+    .min(6, 'Senha deve conter no mínimo 6 caracteres')
+    .required('Senha obrigatória'),
+});
 
 export default function SignUp() {
-  return <div />;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="fullname" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="E-mail" />
+        <Input name="password" type="password" placeholder="Senha" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já possuo login</Link>
+      </Form>
+    </>
+  );
 }
