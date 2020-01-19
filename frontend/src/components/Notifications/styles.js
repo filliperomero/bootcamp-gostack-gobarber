e32dde313e14e3aef78b70867d0a34a4a71f80cd
filliperomero <fillipe.romero@gmail.com>
2020-01-19 15:00:50 -0300

    chore: finish notification

--- frontend/src/components/Notifications/styles.js
+++ frontend/src/components/Notifications/styles.js
@@ -10,6 +10,7 @@ export const Badge = styled.div`
   background: none;
   border: 0;
   position: relative;
+  cursor: pointer;
 
   ${props =>
     props.hasUnread &&
@@ -35,6 +36,7 @@ export const NotificationList = styled.div`
   background: rgba(0, 0, 0, 0.6);
   border-radius: 4px;
   padding: 15px 5px;
+  display: ${props => (props.visible ? 'block' : 'none')};
 
   &::before {
     content: '';
@@ -68,8 +70,10 @@ export const Notification = styled.div`
   }
 
   time {
+    display: block;
     font-size: 12px;
     opacity: 0.6;
+    margin-bottom: 5px;
   }
 
   button {
@@ -77,9 +81,9 @@ export const Notification = styled.div`
     border: 0;
     background: none;
     color: ${lighten(0.2, '#7159c1')};
-    padding: 0 5px;
-    margin: 0 5px;
-    border-left: 1px solid rgba(255, 255, 255, 0.1);
+    /* padding: 0 5px;
+    margin: 0 5px; */
+    /* border-left: 1px solid rgba(255, 255, 255, 0.1); */
   }
 
   ${props =>
@@ -92,6 +96,7 @@ export const Notification = styled.div`
         height: 8px;
         background: #ff892e;
         border-radius: 50%;
+        margin-left: 10px;
       }
     `}
 `;
