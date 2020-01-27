7873dbce21157ed709d396359eafb8e0ddce451a
filliperomero <fillipe.romero@gmail.com>
2020-01-27 20:03:40 -0300

    chore: add Profile avatar system

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,7 +1,50 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { signOut } from '~/store/modules/auth/actions';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import AvatarInput from './AvatarInput';
+
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+
+        <Input name="name" type="text" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Endereço de e-mail" />
+
+        <hr />
+
+        <Input type="password" name="oldPassword" placeholder="Old password" />
+        <Input type="password" name="password" placeholder="New password" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm Password"
+        />
+
+        <button type="submit">Atualizar Perfil</button>
+      </Form>
+
+      <button type="button" onClick={handleSignOut}>
+        Sair do GoBarber
+      </button>
+    </Container>
+  );
 }
